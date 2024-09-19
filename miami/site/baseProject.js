367c110332653eb8cb67ae444ade88f8300a8f80
gabecuscus <gat84@miami.edu>
2024-09-19 19:51:53 -0400

    Lab 4
    We leanred a lot of stuff but w also learned how to use handlebars!!!

---
+++ miami/site/baseProject.js
@@ -0,0 +1,71 @@
+// imports express into our project
+const express = require('express');
+
+const app = express();
+
+
+
+
+// import a package for handlebars
+const expressHandlebars = require('express-handlebars');
+
+// make expresss use the handlebars template engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+
+})  );
+app.set('view engine', 'handlebars');
+
+
+
+
+const PORT = process.env.port || 3000;
+console.log(process.env);
+
+// process routes
+app.get('/', (request,response)=>{
+    response.render('home');
+    
+}  );
+app.get('/about', (request,response)=>{
+    
+    response.render('about');
+}  );
+app.get('/home', (request,response)=>{
+    
+    response.render('home');
+}  );
+
+// this one trigers a serve error------------//
+app.get('/histoty', (requiii,response)=>{    //
+                //
+    response.send('Over hrer are hsitory')   //
+}  );//--------------------------------------//
+
+
+
+
+
+// Handle the error first
+
+//NOT_FOUND      default response
+app.use(  (request,response)=>{
+    
+    response.status(404);
+    
+    response.render('404');
+}  );
+
+//SERVER_ERROR
+app.use(  (error,request,response,next)=>{
+    console.log(error.message);
+    
+    response.status(500);
+
+    response.render('500');
+} );
+
+app.listen(PORT,  ()=>{
+    console.log(`Express is running on http://localhost:${PORT}`);
+    console.log('Press ctrl-c to terminate');
+}  );
