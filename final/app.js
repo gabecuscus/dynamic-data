57dcf39c05b4df2742c2615912ef0e46d3960386
gabecuscus <gat84@miami.edu>
2024-11-26 14:33:26 -0500

    all firstGrade basic function but no home page

---
+++ final/app.js
@@ -0,0 +1,171 @@
+// run the following cmmands in terminal
+
+// sudo npm install express
+// sudo npm install express-handlebars
+
+
+
+// imports express into our project
+const express = require('express');
+
+const app = express();
+
+// specify static routes
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json()); // Parses JSON data
+const handler = require('./lib/handler');
+
+// import a package for handlebars
+const expressHandlebars = require('express-handlebars');
+
+// make expresss use the handlebars template engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+})  );
+
+// app.set('view engine', 'handlebars');
+
+// add the comma money thing
+const hbs = expressHandlebars.create({
+    defaultLayout: 'main',
+    helpers: {
+        formatPrice: function (price) {
+            return price.toLocaleString('en-US'); // Formats with commas
+        },
+    },
+});
+
+// Set up Handlebars engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+const PORT = process.env.port || 3000;
+console.log(process.env);
+
+
+
+
+
+
+
+// render home page
+app.get("/",(req,res)=>{
+    const data = require('./data/homepage.json')
+    res.render('page', {data})
+})
+
+// render about page
+app.get("/about",(req,res)=>{
+    const about = require('./data/about.json')
+    res.render('homepage', {about})
+})
+
+
+
+
+// render category 1 page
+app.get("/category1",(req,res)=>{
+    const aboutBlue = require('./data/blueCars.json')
+    res.render('category', {aboutBlue})
+})
+
+
+// redner category 1 page   lexus
+app.get('/lexus/cat', handler.ren_lexus);
+// render category 2 page    toyota
+app.get('/toyota/cat', handler.ren_toyota);
+// render category 3 page    genesis
+app.get('/genesis/cat', handler.ren_genesis);
+
+// details
+app.get('/lexus/details/:id', handler.ren_lex_details);
+app.get('/toyota/details/:id', handler.ren_toy_details);
+app.get('/genesis/details/:id', handler.ren_gen_details);
+
+app.get('/', (req, res) => {
+    res.render('newsletter-signup', {csrf : 'supersecret'});
+});
+app.post('/process', handler.updateCart); // logner link
+
+
+app.get('/checkout', (req,res)=> {
+    res.render('checkOut', {csrf : 'supersecret'});
+})
+app.post('/checkout/process', handler.ren_checkOut);
+
+app.get('/thank_you', handler.ren_thank_you);
+
+
+
+// details page reference
+app.get("/category1/details/:id",(req,res)=>{
+    const data = require('./data/blueCars.json')
+    // filter it to get only the data that atches the id
+
+    var tempData = {"products":[]}
+
+    data.products = data.products.filter((product)=>{
+        return product.id == req.params.id
+     })
+
+    res.render('details', {data})
+})
+
+
+
+
+
+// cart    Jattin style
+let cart = {"products":[]}
+// !!!!!!!!!
+app.get("/cart",(req,res)=>{
+    // we need to populate cart 
+    // every time we click add to cart
+    if(typeof(req.query.id) != "undefiuned"){
+        //if its not empty
+        cart.products.push(req.query)
+    } else {
+        console.log(req.query.id)
+    }
+    res.render("cart",{"products":cart.products})
+})
+app.get("/carto", (req,res)=> {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+//NOT_FOUND      default response
+app.use(  (request,response)=>{
+    
+    response.status(404);
+    
+    response.render('404');
+}  );
+
+//SERVER_ERROR
+app.use(  (error,request,response,next)=>{
+    console.log(error.message);
+    
+    response.status(500);
+
+    response.render('500');
+} );
+
+app.listen(PORT,  ()=>{
+    console.log(`Express is running on http://localhost:${PORT}`);
+    console.log('Press ctrl-c to terminate');
+}  );
---
+++ final/lib/handler.js
@@ -0,0 +1,252 @@
+// let eList = require('../data/emails.json');
+
+const fs = require("fs");
+
+////////////////
+///////////////         GLOBAL OBJECTS
+/////////////
+const all_the_JSON = {
+    "lexus": require('../data/lexus.json'),
+    "toyota": require('../data/toyota.json'),
+    "genesis": require('../data/genesis.json')
+};
+
+function getRandomCarsObject() {
+    console.log("begin random car search");
+    const selectedCars = {}; // Object to store 6 random cars
+    const brands = Object.keys(all_the_JSON); // ['lexus', 'toyota', 'genesis']
+
+    // Loop until we select 6 unique cars
+    while (Object.keys(selectedCars).length < 6) {
+        // Step 1: Roll dice to choose a random brand
+        const randomBrand = brands[Math.floor(Math.random() * brands.length)];
+        const cars = all_the_JSON[randomBrand].products;
+
+        // Step 2: Roll dice to choose a random car from the selected brand
+        const randomCar = cars[Math.floor(Math.random() * cars.length)];
+
+        // Step 3: Add the car to the selectedCars object if it's not already included
+        if (!selectedCars[randomCar.id]) {
+            // Replace the category with the brand name
+            selectedCars[randomCar.id] = {
+                ...randomCar,
+                category: randomBrand // Replace category with the brand name
+            };
+        }
+    }
+
+    console.log("the 6 cars!!:", selectedCars);
+    return selectedCars; // Return an object with 6 unique cars
+}
+var shopping_cart = {
+    "00": {
+        id: 1,
+        name: "2024 4Runner",
+        price: 40705,
+        category: "toyota",
+        image: "/images/toyota/mains/toy1.png",
+        details: [
+            "16/19 EST. MPG",
+            "ADVENTURE-READY DESIGN",
+            "RUGGED BODY-ON-FRAME CONSTRUCTION"
+        ],
+        gallery: [
+            "/images/toyota/gal/toy1_1.png",
+            "/images/toyota/gal/toy1_2.png",
+            "/images/toyota/gal/toy1_3.png",
+            "/images/toyota/gal/toy1_4.png"
+        ]
+    },
+    "001": {
+        id: 2,
+        name: "2024 Genesis G80",
+        price: 54400,
+        category: "genesis",
+        image: "/images/genesis/mains/gen2.png",
+        details: [
+            "375-HORSEPOWER TURBO V6 ENGINE",
+            "14.5-INCH TOUCHSCREEN DISPLAY",
+            "LUXURIOUS WOOD-TRIMMED INTERIOR"
+        ],
+        gallery: [
+            "/images/genesis/gal/gen2_1.png",
+            "/images/genesis/gal/gen2_2.png",
+            "/images/genesis/gal/gen2_3.png",
+            "/images/genesis/gal/gen2_4.png"
+        ]
+    }
+};
+function calculateTotalPrice(cart) {
+    let total = 0;
+
+    // Iterate through the cart object
+    for (const key in cart) {
+        if (cart.hasOwnProperty(key)) {
+            total += cart[key].price; // Add the price of each car
+        }
+    }
+
+    return total; // Return the total price
+}
+///////////////
+/////////////
+///////////
+
+
+
+
+
+
+
+
+exports.ren_lexus = (req,res)=> {
+    const data = require('../data/lexus.json');
+
+    res.render('category', {data});
+}
+
+exports.ren_toyota = (req,res)=> {
+    const data = require('../data/toyota.json');
+
+    res.render('category', {data});
+}
+
+exports.ren_genesis = (req,res)=> {
+    const data = require('../data/genesis.json');
+
+    res.render('category', {data});
+}
+
+
+
+
+exports.ren_thank_you = (req,res)=> {
+    
+    var total_price = calculateTotalPrice(shopping_cart);
+
+
+
+
+    res.render('thankYou', {total_price, perosnal_info});
+}
+
+exports.ren_lex_details = (req, res)=> {
+    const data = require('../data/lexus.json');
+
+    // console.log(data.id);
+    console.log(data.company);
+    console.log("imma take  a ");
+    console.log(req.params.id);
+    // console.log(data.products.)
+    
+    // appearntly find is faster than filter, because filter will return an array, find will do the first one and hten stop searching
+    let current_car = data.products.find((prod)=> prod.id==req.params.id);
+
+    // lesgoo it works!!!!!!!!
+    console.log(current_car.name);
+
+    //globals
+    console.log("the shoping cart!!!");
+    console.log(shopping_cart);
+    var total_price = calculateTotalPrice(shopping_cart);
+    var sixRandomCars = getRandomCarsObject();
+    res.render('details', {current_car, sixRandomCars, shopping_cart, total_price});
+} 
+exports.ren_toy_details = (req, res)=> {
+    const data = require('../data/toyota.json');
+    let current_car = data.products.find((prod)=> prod.id==req.params.id);
+    
+    var sixRandomCars = getRandomCarsObject();
+    var total_price = calculateTotalPrice(shopping_cart);
+    res.render('details', {current_car, sixRandomCars, shopping_cart, total_price});
+}
+exports.ren_gen_details = (req, res)=> {
+    const data = require('../data/genesis.json');
+    let current_car = data.products.find((prod)=> prod.id==req.params.id);
+    
+    var sixRandomCars = getRandomCarsObject();
+    var total_price = calculateTotalPrice(shopping_cart);
+    res.render('details', {current_car, sixRandomCars, shopping_cart, total_price});
+}
+
+
+//cart    maybe.......
+// var de_cart = {"products": []};
+// shopping_cart
+exports.ren_cart = (req, res)=> {
+    if(typeof(req.query.id) != "undefiuned"){
+        //if its not empty
+        de_cart.products.push(req.query);
+        } else {
+            console.log(req.query.id);
+        }
+    console.log("DEEEE CARRRTTTTT");
+    console.log(de_cart);
+    res.render("cart", {de_cart});
+}
+
+
+
+
+let cartCounter = 0;
+exports.updateCart = (req, res)=> {
+    console.log(" infromation gathered!!!");
+    console.log(req.body);
+    
+    
+    // if category 1 then categoryString equals lexus
+    // if categroy 2 then categoryString equals toyota
+    // if categroy 3 then categoryString equals genesis
+    // Step 1: Map category number to company name
+    const categoryMap = {
+        1: "lexus",
+        2: "toyota",
+        3: "genesis"
+    };
+
+    const companyName = categoryMap[req.body.category]; // Convert category to company name
+    if (!companyName) {
+        console.log("Invalid category received:", req.body.category);
+        return res.status(400).send("Invalid category");
+    }
+
+    // let current_car = all_the_JSON.companyName.products.find((prod)=> prod.id==req.body.id);
+    // const carsArray = all_the_JSON[companyName]?.products; // Get the array of products for the company
+    // const current_car = carsArray.find(car => car.id == req.body.id); // Find the car by ID
+    var current_car = all_the_JSON[companyName]?.products.find((prod) => prod.id == req.body.id);
+    current_car.category = companyName;
+
+    cartCounter++; 
+    shopping_cart[String(cartCounter)] = current_car;
+    console.log("the shopping cart after the push");
+    console.log(shopping_cart);
+
+}
+var perosnal_info = {};
+exports.ren_checkOut = (req,res)=> {
+    console.log(" shipping infromation gathered!!!");
+    console.log(req.body);
+
+    perosnal_info = req.body;
+    res.redirect('/thank_you');
+
+}
+
+
+
+
+
+// ------------- EXPORTS page -----------------------
+exports.ren_lexus = this.ren_lexus;
+exports.ren_toyota = this.ren_toyota;
+exports.ren_genesis = this.ren_genesis;
+
+exports.ren_lex_details = this.ren_lex_details;
+exports.ren_toy_details = this.ren_toy_details;
+exports.ren_gen_details = this.ren_gen_details;
+
+exports.ren_cart = this.ren_cart;//dont use this
+exports.updateCart = this.updateCart;
+exports.ren_checkOut = this.ren_checkOut;
+
+exports.ren_thank_you = this.ren_thank_you;
